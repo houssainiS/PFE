6fc179ec154136788adfb87acbbc6fc36a27b333
houssainiS <houssaini.slimen@gmail.com>
2025-02-16 22:45:20 +0100

    added empety pages for simple and advanced mode

--- work/views.py
+++ work/views.py
@@ -5,3 +5,10 @@ from login.models import User
 def home(request, user_id):
     user = User.objects.get(id=user_id)  # Get the user by ID using your custom model
     return render(request, 'work/home.html', {'user': user})
+def simple_mode(request, user_id):
+    user = User.objects.get(id=user_id)  # Get the user by ID using your custom model
+    return render(request, 'work/simpleMode.html', {'user': user})
+
+def advanced_mode(request, user_id):
+    user = User.objects.get(id=user_id)  # Get the user by ID using your custom model
+    return render(request, 'work/advancedMode.html', {'user': user})
