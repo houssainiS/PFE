fc142beff1c687d607403cedcfb9fa827946b28d
houssainiS <houssaini.slimen@gmail.com>
2025-04-17 14:10:38 +0100

    added some secrets into .env

--- pfe/settings.py
+++ pfe/settings.py
@@ -17,17 +17,30 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+
+
+#env setup
+import environ
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v!-hd#%6bq1137$gegf+$#5@=ppp716&x160*o%5=$*^=qnw*^'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 # Application definition
 
