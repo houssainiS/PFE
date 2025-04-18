8661e3f09aec8df839a5eed2d1cdc13ec3508706
houssainiS <houssaini.slimen@gmail.com>
2025-04-18 17:31:47 +0100

    fixed the logout for django update 5.2

--- work/urls.py
+++ work/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
 from . import views
 app_name = 'work' 
 
 urlpatterns = [
     path('<int:user_id>/', views.home, name='home'),  # Accept user_id in the URL
-    path('logout/', LogoutView.as_view(next_page='login:index'), name='logout'),  # Logout view
     path('<int:user_id>/advancedMode/', views.advanced_mode , name='advanced_mode'),
     path('<int:user_id>/advancedMode/getResponse/',views.get_ai_response, name='get_ai_response'),
     path('save/', views.save_generated_website, name='save_generated_website'),
